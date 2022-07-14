1debef2e1b0b046879eefd6f83fab6592580f69a
hxse <narutohy@163.com>
2022-07-14 15:17:28 +0800

    update

---
+++ assets/cvtw/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>
+        <button
+          className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          type="button"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is: {count}
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default App;
--- my_init.ps1
+++ my_init.ps1
@@ -1,21 +1,66 @@
 # 用powershell如下调用来初始化工作空间
 # powershell.exe -noexit '. D:\\Note\\02-Computer\\program\\my_init.ps1'
 # in windows terminal: powershell -noexit '. C:\Users\hxse\Downloads\my_init.ps1'
+#chcp 65001
+
+$ompPath= "C:\Users\hxse\scoop\apps\oh-my-posh\current\themes"
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh --init --shell pwsh --config $ompPath\kali2.omp.json | Invoke-Expression #预览参考: https://ohmyposh.dev/docs/themes
 
 $scriptPath = $MyInvocation.MyCommand.Definition
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$assetsDir= $scriptDir+'\assets'
 $dir=Get-Location
+echo $assetsDir
 
+# For zoxide v0.8.0+
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
+
+# For older versions of zoxide
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell) -join "`n"
+})
 
 Function nfp {netstat -aon|findstr $args[0]}
 Function tfp {tasklist|findstr $args[0]}
 Function tkp {taskkill /T /F /PID $args[0]}
 Function sd { Shutdown -s -t 30 }
 
-Function cvr { yarn create vite $args[0] --template react }
-Function cvt { npx degit joaopaulomoraes/reactjs-vite-tailwindcss-boilerplate $args[0]}
+$ytw=[regex]::Unescape("\u4ee5\u592a\u7f51")#以太网三个字的的unicode
+$mip = Get-NetIPAddress |  where IPAddress -like '192.168.*' | where InterfaceAlias -like $ytw | select  -ExpandProperty "IPAddress"
+Function mip{$mip}
 
-$cwaDir="E:\App\proxy\clash-web-app"
+
+Function cvr { yarn create vite $args[0] --template react;
+				cd $args[0];
+				yarn;
+				}
+Function cvt { yarn create vite $args[0] --template react-ts;
+				cd $args[0];
+				yarn;
+				}
+Function cvtw { yarn create vite $args[0] --template react-ts;
+				cd $args[0];
+				yarn;
+				# https://tailwindcss.com/docs/guides/vite
+				$name=$args[0]
+				yarn add -D tailwindcss postcss autoprefixer;
+				npx tailwindcss init -p;
+				Copy-Item -Path $assetsDir'\cvtw\tailwind.config.js' -Destination $dir\$name\'tailwind.config.js'
+				Copy-Item -Path $assetsDir'\cvtw\index.css' -Destination $dir\$name\'src\index.css'
+				Copy-Item -Path $assetsDir'\cvtw\App.tsx' -Destination $dir\$name\'src\App.tsx'
+				
+				 #(Get-Content ".\tailwind.config.js" | Out-String) -replace "content: \[]", "content:[`r`n    `"./index.html`",`r`n    `"./src/**/*.{vue,js,ts,jsx,tsx}`",`r`n  ]" | Out-File ".\tailwind.config.js";
+				 #"@tailwind base;@tailwind components;@tailwind utilities;"-replace ";", ";`r`n"|Out-File ".\src\index.css";
+				 #(Get-Content ".\src\App.tsx" | Out-String ) -replace "(?sm)return \(.*\)", "return (`r`n    <h1 className=`"text-3xl font-bold underline`">`r`n      Hello world!`r`n    </h1>`r`n  )" | Out-File ".\src\App.tsx";
+				}
+
+
+$cwaDir="D:\App\proxy\clash-web-app"
 Function cwa { & $cwaDir\cwa.bat $args}
 Function cwas {  cat $cwaDir\config\config.json | jq '.origin' }
 
@@ -40,8 +85,8 @@ $cookie=""#"--cookies-from-browser","chrome"
 $ytDownload="D:\my_repo\parrot_fashion\download"
 Function yts {
 $dir=Get-Location;
-cd $ytDownload; 
-& python D:\my_repo\parrot_fashion\crawler\loop_trans_srt.py --dirPath $ytDownload $args;
+cd "D:\my_repo\parrot_fashion\crawler";
+& pdm run python D:\my_repo\parrot_fashion\crawler\loop_trans_srt.py ts --dirPath $ytDownload $args;
 cd $dir
 }
 Function yvv { 
@@ -76,12 +121,17 @@ Function ypat{ypa $args;yts $args;}
 Function ti { python "D:\Note\02-Computer\program\python\python-repo\color-filter\cf.py" ti $args }
 Function pi { python "D:\Note\02-Computer\program\python\python-repo\color-filter\cf.py" pi $args }
 
-Function js { powershell "E:\91porn\run_94.bat" $args}
+Function js { 
+	$dir=Get-Location;
+	cd "D:\my_repo\94_crawler"
+	pdm run python ".\94.py" md --outPath "E:/91porn" $args
+	cd $dir
+	}
 
 Function jable {
 $dir=Get-Location;
-cd "E:\jable download"; 
-python "D:\my_repo\JableTVDownload\main.py" $args;
+cd "D:\my_repo\JableTVDownload"
+pdm run python "D:\my_repo\JableTVDownload\main.py" --outPath "E:\jable download" $args;
 cd $dir
 }
 
