2aed62c10eee3e47d570bc4a3af0608fecf017e5
hxse <narutohy@163.com>
2022-12-26 01:28:28 +0800

    update

--- my_init.sh
+++ my_init.sh
@@ -1,7 +1,8 @@
+# C:\Users\hxse\scoop\apps\git\current\bin\bash.exe  --login -i -c '"D:\my_repo\my_cmd\my_init.sh"'
 export mypath=$(dirname $0)
 export myname=$(basename $0)
 export my="sdf"
 echo ${mypath}
 echo $myname
 echo $my
-exec /bin/bash
+exec bash
