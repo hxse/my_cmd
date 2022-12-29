fbbd01aa84c2ac05d7287759079a4c80a38d92fe
hxse <narutohy@163.com>
2022-12-29 23:57:18 +0800

    update

---
+++ script/clash_control.ps1
@@ -0,0 +1,3 @@
+Set-PSDebug -Trace 1
+cd "D:\App\proxy\clash-dashboard\dist"
+. "C:\Users\hxse\scoop\apps\python\current\python.exe" -m "http.server" 9091
---
+++ script/subcovert.ps1
@@ -0,0 +1,3 @@
+Set-PSDebug -Trace 1
+cd "D:\App\proxy\subconverter\subconverter"
+. "D:\App\proxy\subconverter\subconverter\subconverter.exe"
