8aaebd8932d72854d3f2665bec1939f99540bef7
hxse <narutohy@163.com>
2022-12-31 01:43:58 +0800

    update

--- xonsh_script/run_windows_terminal.bat
+++ xonsh_script/run_windows_terminal.bat
@@ -2,7 +2,7 @@
 :: wt -w 0 new-tab --title "subcovert" -p "xonsh" cmd /K "D:\my_repo\my_cmd\xonsh_script\run_xonsh_command.bat" "echo haha"
 :: wt -w 0 new-tab --title "subcovert" -p "xonsh" cmd /K "D:\my_repo\my_cmd\xonsh_script\run_xonsh.bat" "D:\my_repo\my_cmd\xonsh_script\test 2.xsh"
 
-::用cmd
+::用cmd,引号的转义很麻烦,脚本文件和项目文件夹都尽量用下划线去代替空格
 wt -w 0 new-tab --title "subcovert" -p "cmd" cmd /K "D:\my_repo\my_cmd\xonsh_script\run_xonsh_command.bat" "cat r'D:\my_repo\my_cmd\xonsh_script\run_windows_terminal.bat'"
 timeout 1
 wt -w 0 new-tab --title "subcovert" -p "cmd" cmd /K "D:\my_repo\my_cmd\xonsh_script\run_xonsh.bat" "D:\my_repo\my_cmd\xonsh_script\scirpt\subcovert.xsh"
