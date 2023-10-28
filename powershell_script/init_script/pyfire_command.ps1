38f51f002cccc90797e03dddfbb3bb609cfdb2b0
hxse <narutohy@163.com>
2023-10-28 18:08:47 +0800

    update

--- powershell_script/init_script/pyfire_command.ps1
+++ powershell_script/init_script/pyfire_command.ps1
@@ -2,89 +2,89 @@
 
 function py_ypa {
     # 替换参数示例: py_ypa newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_ypa.py" ypa $args
 }
 
 
 function py_ypa_bbc {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_ypa.py" ypa_bbc $args
 }
 
 
 function py_ypa_kur {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_ypa.py" ypa_kur $args
 }
 
 
 function py_ypa_bs {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_ypa.py" ypa_bs $args
 }
 
 
 function py_lw {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_lw.py" py_lw $args
 }
 
 
 function py_lw_bbc {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_lw.py" py_lw_bbc $args
 }
 
 function py_lw_kur {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_lw.py" py_lw_kur $args
 }
 
 function py_lw_bs {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_lw.py" py_lw_bs $args
 }
 
 function py_lw_dh {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_lw.py" py_lw_dh $args
 }
 
 function py_mzf {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_mzf.py" mzf $args
 }
 
 function py_mzf_bbc {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_mzf.py" mzf_bbc $args
 }
 
 function py_mzf_kur {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_mzf.py" mzf_kur $args
 }
 
 function py_mzf_bs {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_mzf.py" mzf_bs $args
 }
 
 function py_mzf_dh {
     # 替换参数示例: py_ypa_bbc newUrl --proxy '--proxy \"new proxy\"'
-    cd D:\my_repo\my_cmd\python_script
+    cd "D:\my_repo\my_cmd\python_script"
     pdm run python "D:\my_repo\my_cmd\python_script\py_simple_fire\run_mzf.py" mzf_dh $args
 }
