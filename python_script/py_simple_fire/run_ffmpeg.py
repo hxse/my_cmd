e9cb27c8c6047c2c2d14f9ab55394b7a44e7d3c1
hxse <narutohy@163.com>
2023-11-15 13:42:00 +0800

    udpate

--- python_script/py_simple_fire/run_ffmpeg.py
+++ python_script/py_simple_fire/run_ffmpeg.py
@@ -12,7 +12,7 @@ from pathlib import Path
 def convertFile(filePath, suffix=".mp4", enableCopy=True):
     filePath = Path(filePath)
     outPath = filePath.parent / (filePath.stem + suffix)
-    command = f'ffmpeg -i "{filePath}" {"-acodec copy -vcodec copy"if enableCopy else ""} "{outPath}"'
+    command = f'ffmpeg -i "{filePath}" {"-vn" if suffix==".ogg" else ""} {"-acodec copy -vcodec copy" if enableCopy in [True, "1", 1] else ""} "{outPath}"'
     print(command)
     subprocess.run(command, cwd=filePath.parent)
 
