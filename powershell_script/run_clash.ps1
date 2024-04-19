688ce44f0ab6c6005a596c1278f13c98d86e18d4
hxse <narutohy@163.com>
2024-04-20 00:06:48 +0800

    update

--- powershell_script/run_clash.ps1
+++ powershell_script/run_clash.ps1
@@ -8,7 +8,8 @@ Function e {
     # $clashPathDir = "D:\App\proxy\clash-premium"
     # $clashPath = "D:\App\proxy\clash-premium\clash-windows-386.exe"
     $clashPathDir = "D:\App\proxy\clash-meta"
-    $clashPath = "D:\App\proxy\clash-meta\clash.meta-windows-amd64-compatible.exe"
+    # $clashPath = "D:\App\proxy\clash-meta\clash.meta-windows-amd64-compatible.exe"
+    $clashPath = "D:\App\proxy\clash-meta\mihomo-windows-amd64-compatible-go120.exe"
 
     $clashDir = "D:\my_repo\clash-rule-providers"
     $uiPath = "D:\App\proxy\yacd-gh-pages\yacd-gh-pages"
