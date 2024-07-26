a0c8864bf674f87a41621f66ece020a548c6fe51
hxse <narutohy@163.com>
2024-07-27 02:01:21 +0800

    update

--- powershell_script/run_wt_powershell.ps1
+++ powershell_script/run_wt_powershell.ps1
@@ -6,40 +6,40 @@ if ($code -eq 0) {
     return
 }
 
-# powershell -Command "Start-Sleep -Seconds 0.6"
+# powershell -Command "Start-Sleep -Seconds 0.5"
 # wt -w 0 new-tab --title "sync_note" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\run_sync_note.ps1"
 
-powershell -Command "Start-Sleep -Seconds 0.6"
+powershell -Command "Start-Sleep -Seconds 0.5"
 wt -w 0 new-tab --title "subcovert" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\run_subcovert.ps1"
 
-powershell -Command "Start-Sleep -Seconds 0.6"
+powershell -Command "Start-Sleep -Seconds 0.5"
 wt -w 0 new-tab --title "ftp" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\run_ftp.ps1"
 
-powershell -Command "Start-Sleep -Seconds 0.6"
+powershell -Command "Start-Sleep -Seconds 0.5"
 wt -w 0 new-tab --title "ftp" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\run_ftp2.ps1"
 
-powershell -Command "Start-Sleep -Seconds 0.6"
+powershell -Command "Start-Sleep -Seconds 0.5"
 wt -w 0 new-tab --title "webdav" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\run_webdav_config.ps1"
 
-powershell -Command "Start-Sleep -Seconds 0.6"
+powershell -Command "Start-Sleep -Seconds 0.5"
 wt -w 0 new-tab --title "webdav" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\run_webdav.ps1"
 
-powershell -Command "Start-Sleep -Seconds 0.6"
+powershell -Command "Start-Sleep -Seconds 0.5"
 wt -w 0 new-tab --title "webdav" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\run_webdav2.ps1"
 
-powershell -Command "Start-Sleep -Seconds 0.6"
+powershell -Command "Start-Sleep -Seconds 0.5"
 wt -w 0 new-tab --title "player" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\run_player.ps1"
 
-powershell -Command "Start-Sleep -Seconds 0.6"
+powershell -Command "Start-Sleep -Seconds 0.5"
 wt -w 0 new-tab --title "alist" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\run_alist.ps1"
 
-powershell -Command "Start-Sleep -Seconds 0.6"
+powershell -Command "Start-Sleep -Seconds 0.5"
 wt -w 0 new-tab --title "active_windows_keyboard" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\active_windows_keyboard.ps1"
 
-# powershell -Command "Start-Sleep -Seconds 0.6"
+# powershell -Command "Start-Sleep -Seconds 0.5"
 # wt -w 0 new-tab --title "run_clash_providers" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\run_clash_providers.ps1"
 
-# powershell -Command "Start-Sleep -Seconds 0.6"
+# powershell -Command "Start-Sleep -Seconds 0.5"
 # wt -w 0 new-tab --title "run_clash" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\;. D:\my_repo\my_cmd\powershell_script\run_clash.ps1"
 
 $code = (Start-Process -FilePath "autohotkey.exe" -Args "D:\my_repo\my_cmd\ahk_script\check_exist.ahk `"^active_windows_keyboard$`" `"^C:\\Users\\hxse\\scoop\\apps\\windows-terminal\\.*\\WindowsTerminal.exe$`" 180"  -PassThru -Wait  -WindowStyle Minimized).exitCode
@@ -48,7 +48,7 @@ if ($code -eq 0) {
     return
 }
 
-powershell -Command "Start-Sleep -Seconds 0.6"
+powershell -Command "Start-Sleep -Seconds 0.5"
 # https://github.com/PowerShell/PowerShell/issues/3028
 # D:\my_repo\my_cmd\powershell_script\run-hidden.exe PowerShell  -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process D:\my_repo\my_cmd\powershell_script\run-hidden.exe -ArgumentList 'PowerShell -WindowStyle Hidden -NoProfile   -ExecutionPolicy Bypass -File """D:\my_repo\my_cmd\powershell_script\run_minimize.ps1"""'}"
 D:\my_repo\my_cmd\powershell_script\run-ps.vbs D:\my_repo\my_cmd\powershell_script\run_minimize.ps1
