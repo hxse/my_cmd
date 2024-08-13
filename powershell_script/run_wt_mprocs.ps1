9f866a5a30a0912cb5b6eaca0a30e4bed985e0d0
hxse <narutohy@163.com>
2024-08-13 09:52:27 +0800

    update

--- powershell_script/run_wt_mprocs.ps1
+++ powershell_script/run_wt_mprocs.ps1
@@ -10,7 +10,6 @@ if ($code -eq 0) {
 powershell -Command "Start-Sleep -Seconds 0.5"
 wt -w 0 new-tab --title "mprocs" -p "powershell" powershell -NOLogo -NoExit -Command ". D:\my_repo\my_cmd\powershell_script\my_init.ps1\; mprocs -c 'D:\my_repo\my_cmd\mprocs\mprocs.yaml'"
 
-powershell -Command "Start-Sleep -Seconds 0.5"
 # https://github.com/PowerShell/PowerShell/issues/3028
 # D:\my_repo\my_cmd\powershell_script\run-hidden.exe PowerShell  -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process D:\my_repo\my_cmd\powershell_script\run-hidden.exe -ArgumentList 'PowerShell -WindowStyle Hidden -NoProfile   -ExecutionPolicy Bypass -File """D:\my_repo\my_cmd\powershell_script\run_minimize.ps1"""'}"
 D:\my_repo\my_cmd\powershell_script\run-ps.vbs D:\my_repo\my_cmd\powershell_script\run_minimize.ps1
